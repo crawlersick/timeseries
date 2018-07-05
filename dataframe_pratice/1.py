23b3c7aa3e1c753d59319f4ed4d6763eb5076dd9
john <johnbyyang@gmail.com>
2018-07-05 08:34:27 +0800

    ..

---
+++ dataframe_pratice/1.py
@@ -0,0 +1,10 @@
+import pandas as pd
+df=pd.DataFrame([1,2,3,4,5],
+        columns=['cols'],
+        index=['a','b','c','d','e'])
+print(df)
+
+df2=pd.DataFrame([[1,2,3],[4,5,6]],
+        columns=['col1','col2','col3'],
+        index=['a','b'])
+print(df2)
